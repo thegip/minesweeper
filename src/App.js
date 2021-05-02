e2b274185caccb0a8f65656290e78bba3e3bf67e
Viktor Gip <theviktorgip@gmail.com>
2021-05-02 09:45:53 -0700

    1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>MINESWEEEPEERRR POGGERGES</h1>
+        <div class="gamearea">
+          <div>header board</div>
+          <div>
+            square place 
+            <br/>
+            100 squares
+          </div>
+        </div>
       </header>
     </div>
   );
