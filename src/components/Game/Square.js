262c870ba441656162352960d37692c94e8bc5ce
Viktor Gip <theviktorgip@gmail.com>
2021-06-01 22:12:45 +0200

    conditional added to squareJS

--- src/components/Game/Square.js
+++ src/components/Game/Square.js
@@ -2,7 +2,14 @@ import './Game.css'
 
 function Square(props){
     console.log(props)
-    return <button className="square" onClick={() => { console.log(props.square.id) }}>{props.square.id}</button>
+    if(props.square.bomb===true){
+        return <button className="square" onClick={() => { console.log(props.square.id) }}>bomb</button>
+    }
+
+    if(props.square.bomb===false){
+        return <button className="square" onClick={() => { console.log(props.square.id) }}>{props.square.id}</button>
+    }
+    
 }
 
 export default Square
