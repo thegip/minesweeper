817a423a8f38010b073329fc05eff57a0ebcaabe
Viktor Gip <theviktorgip@gmail.com>
2021-06-04 12:36:57 -0700

    state

--- src/components/Game/Square.js
+++ src/components/Game/Square.js
@@ -3,8 +3,11 @@ import {useState} from 'react'
 
 function Square(props){
     console.log(props)
+    const [count, setCount] = useState(0);
+
+
     if(props.square.bomb===true){
-        return <button className="square" onClick={() => { console.log(props.square.id) }}>bomb</button>
+        return <button className="square" onClick={() => setCount(count + 1)}>jeff {count}</button>
     }
 
     if(props.square.bomb===false){
