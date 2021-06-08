d0b2cf10e0efdc26c303f067de24a0151471a984
Viktor Gip <theviktorgip@gmail.com>
2021-06-08 07:18:26 -0700

    style change onClick

--- src/components/Game/Square.js
+++ src/components/Game/Square.js
@@ -4,16 +4,33 @@ import {useState} from 'react'
 function Square(props){
     console.log(props)
     const [count, setCount] = useState(0);
+    const [color, setColor] = useState("blue");
+
+    let style = {
+        color: color
+    }
 
 
     if(props.square.bomb===true){
-        return <button className="square" onClick={() => setCount(count + 1)}>jeff {count}</button>
+        return <button style={style} className="square" onClick={ToggleClicked}>jeff {count}</button>
     }
 
     if(props.square.bomb===false){
         return <button className="square" onClick={() => { console.log(props.square.id) }}>{props.square.id}</button>
     }
-    
+
+    function ToggleClicked(){
+        if (color === "blue" ){
+            setColor("red")
+           
+        }
+        if (color === "red" ){
+            setColor("blue")
+            
+        }
+    }
 }
+
+
 
 export default Square
