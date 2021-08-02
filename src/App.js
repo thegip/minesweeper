baff78a96f624d675da2721d7a452d0d1c97bcdc
Viktor Gip <theviktorgip@gmail.com>
2021-08-02 12:32:24 +0200

    added game toggle

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import Info from './components/InfoContent/InfoContent'
 import './Normalize.css'
 import './App.css'
 import {GenerateBoard} from './components/GenerateBoard'
+import { useState } from 'react'
 
 const board = GenerateBoard(10,10,10)
 console.log(board)
 
+
+
 function App() {
+  const [showGame, setShowGame] = useState(false)
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <Info/>
-     
-      <Game board={board}/>
+      <button onClick={() => setShowGame(true)} >show game</button>
+      {showGame && <Game board={board}/>}
       <div>Project information</div>
     </div>
   )
