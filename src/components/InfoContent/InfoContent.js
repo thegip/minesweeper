c27d1c6e39cab64f607ac0d820150a6b93720e79
Viktor Gip <theviktorgip@gmail.com>
2021-08-04 13:00:31 +0200

    button styling mostly

--- src/components/InfoContent/InfoContent.js
+++ src/components/InfoContent/InfoContent.js
@@ -1,16 +1,16 @@
-
+import "./InfoContent.css"
 function Info() {
     return(
         <div>
-            <h1>Viktor Gip</h1>
+            <h1 className="test5">Viktor Gip</h1>
             <h3>Frontend Developer</h3>
             <p>
                 This site is a work in progress
                 More information will be available soon    
             </p>
             <div>
-                <button>LinkedIn</button>
-                <button>Github</button>
+                <button className="button1">LinkedIn</button>
+                <button className="button1">Github</button>
             </div>
             <br/>
         </div>
