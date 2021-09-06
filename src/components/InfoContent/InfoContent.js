5ac114dee0efc4ec862dbe1df8b0448b7f089187
Viktor Gip <theviktorgip@gmail.com>
2021-09-06 16:05:18 +0200

    just content

--- src/components/InfoContent/InfoContent.js
+++ src/components/InfoContent/InfoContent.js
@@ -3,15 +3,15 @@ function Info() {
     return(
         <div>
             <h1 className="test5">Viktor Gip</h1>
-            <h3>Frontend Developer</h3>
-            <p>
-                This site is a work in progress
-                More information will be available soon    
-            </p>
+            <h3>Frontend Developer with a passion for tech and design</h3>
             <div>
                 <button className="button1">Contact</button>
                 <button className="button1">LinkedIn</button>
                 <button className="button1">Github</button>
+            </div>
+            <div>
+                <button className="button1">disign work</button>
+                <button className="button1">development work</button>
             </div>
             <br/>
         </div>
