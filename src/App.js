91225b0167ea0ccba9c6543e327757fd5a2717a6
Viktor Gip <theviktorgip@gmail.com>
2022-02-17 12:09:12 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import Game from './components/Game/Game'
 import Info from './components/InfoContent/InfoContent'
 import './Normalize.css'
 import './App.css'
-
 import { useState } from 'react'
 
-
-
-
 function App() {
+
   const [showGame, setShowGame] = useState(false)
+  
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <Info/>
       <h3 className="showGame" onClick={() => setShowGame(!showGame)}>Click to play Minesweeper!</h3>
-      
       {showGame && <Game/>}
       <div>Github for this website</div>
     </div>
